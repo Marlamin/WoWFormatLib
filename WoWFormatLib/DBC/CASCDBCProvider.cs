bec738c0fe1ee7a11150126a88baa7a5395a81e4
Martin Benjamins <ictmartin@gmail.com>
2019-09-30 12:00:05 +0200

    Add DBCD submodule/CASC DBC provider

---
+++ WoWFormatLib/DBC/CASCDBCProvider.cs
@@ -0,0 +1,21 @@
+﻿using DBCD.Providers;
+using System.IO;
+using WoWFormatLib.Utils;
+
+namespace WoWExportTools.DBC
+{
+    class CASCDBCProvider : IDBCProvider
+    {
+        public Stream StreamForTableName(string tableName, string build)
+        {
+            if (Listfile.TryGetFileDataID("dbfilesclient/" + tableName + ".db2", out var fileDataID))
+            {
+                return CASC.OpenFile(fileDataID);
+            }
+            else
+            {
+                throw new FileNotFoundException("DBC " + tableName + " not found in listfile, could not look up filedataid!");
+            }
+        }
+    }
+}
