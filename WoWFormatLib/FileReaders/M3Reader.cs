0d0d0486971dcb301217e695df86fe363fb798f8
Martin Benjamins <ictmartin@gmail.com>
2025-06-21 00:53:16 +0200

    WIP M3 support

---
+++ WoWFormatLib/FileReaders/M3Reader.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using System.Text;
+using WoWFormatLib.FileProviders;
+using WoWFormatLib.Structs.M3;
+
+namespace WoWFormatLib.FileReaders
+{
+    public class M3Reader
+    {
+        public M3Model model;
+
+        public void LoadM3(string filename, bool loadSkins = true)
+        {
+            if (FileProvider.FileExists(filename))
+            {
+                LoadM3(FileProvider.GetFileDataIdByName(Path.ChangeExtension(filename, "M3")), loadSkins);
+            }
+            else
+            {
+                throw new FileNotFoundException("M3 " + filename + " not found");
+            }
+        }
+
+        public void LoadM3(uint fileDataID, bool loadSkins = true)
+        {
+#if DEBUG
+            using (var stream = FileProvider.OpenFile(fileDataID))
+            {
+                LoadM3(stream, loadSkins);
+            }
+#else
+                try
+                {
+                    LoadM3(FileProvider.OpenFile(fileDataID));
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Error during reading file: {0}", e.Message);
+                    return;
+                }
+#endif
+        }
+
+        public void LoadM3(Stream m3, bool loadSkins = true)
+        {
+            using (var bin = new BinaryReader(m3))
+            {
+                while (bin.BaseStream.Position < m3.Length)
+                {
+                    var chunkName = (M3Chunks)bin.ReadUInt32();
+                    if (chunkName == 0)
+                        throw new Exception("M3 is likely encrypted");
+
+                    var debugChunkName = Encoding.ASCII.GetString(BitConverter.GetBytes((uint)chunkName));
+
+                    var chunkSize = bin.ReadUInt32();
+
+                    var propertyA = bin.ReadUInt32();
+                    var propertyB = bin.ReadUInt32();
+
+                    var prevPos = bin.BaseStream.Position;
+
+                    Console.WriteLine("M3 chunk " + debugChunkName + " of size " + chunkSize);
+                    switch (chunkName)
+                    {
+                        case M3Chunks.M3DT:
+                            model.Header = new M3DT()
+                            {
+                                PropertyA = propertyA,
+                                PropertyB = propertyB,
+
+                                Unknown0 = bin.ReadUInt32(),
+                                Unknown1 = bin.ReadUInt32(),
+                                Unknown2 = bin.ReadUInt32(),
+                                Unknown3 = bin.ReadUInt32(),
+                                Unknown4 = bin.ReadUInt32(),
+                                Unknown5 = bin.ReadUInt32(),
+                                Unknown6 = bin.ReadUInt32(),
+
+                                BoundingBoxMin = new Vector3(bin.ReadSingle(), bin.ReadSingle(), bin.ReadSingle()),
+                                BoundingBoxMax = new Vector3(bin.ReadSingle(), bin.ReadSingle(), bin.ReadSingle()),
+                                Radius = bin.ReadSingle(),
+
+                                BoundingBox2Min = new Vector3(bin.ReadSingle(), bin.ReadSingle(), bin.ReadSingle()),
+                                BoundingBox2Max = new Vector3(bin.ReadSingle(), bin.ReadSingle(), bin.ReadSingle()),
+                                Radius2 = bin.ReadSingle(),
+
+                                Unknown7 = bin.ReadUInt32()
+                            };
+                            break;
+                        case M3Chunks.M3SI:
+                            model.Instances = ReadM3SIChunk(bin, chunkSize, propertyA, propertyB);
+                            break;
+                        case M3Chunks.MES3:
+                            model.Mesh = ReadMES3Chunk(bin, chunkSize, propertyA, propertyB);
+                            break;
+                        case M3Chunks.M3CL:
+                            model.Collision = ReadM3CLChunk(bin, chunkSize, propertyA, propertyB);
+                            break;
+                        default:
+                            Console.WriteLine("Unknown M3 chunk: {0} of size {1} at position {2}", debugChunkName, chunkSize, bin.BaseStream.Position);
+                            bin.BaseStream.Position += chunkSize;
+                            break;
+                    }
+
+                    // Ensure we've read everything
+                    var correctPos = prevPos + chunkSize;
+                    if (bin.BaseStream.Position != correctPos)
+                    {
+                        Console.WriteLine("Ended up at wrong position after reading " + debugChunkName + ": " + bin.BaseStream.Position + " != " + correctPos);
+                        bin.BaseStream.Position = correctPos;
+                    }
+                }
+            }
+        }
+
+        public M3SI ReadM3SIChunk(BinaryReader bin, uint chunkSize, uint propertyA, uint propertyB)
+        {
+            var instances = new M3SI();
+
+            instances.PropertyA = propertyA;
+            instances.PropertyB = propertyB;
+
+            var baseOffset = bin.BaseStream.Position;
+
+            instances.Magic = new string(bin.ReadChars(4));
+            if (instances.Magic != "m3IS")
+                throw new Exception("Invalid M3 instances magic!");
+
+            instances.Version = bin.ReadUInt32();
+            if (instances.Version != 2)
+                throw new Exception("Unsupported M3 instance version: " + instances.Version);
+
+            instances.TotalSize = bin.ReadUInt32() * 4; // Total Chunk size
+            instances.Unknown1 = bin.ReadUInt32();
+
+            var instanceSize = bin.ReadUInt32();
+            var maybeNotInstanceOffset = bin.ReadUInt32() * 4;
+
+            instances.Instances = new M3Instance[instanceSize];
+
+            for (int i = 0; i < instanceSize; i++)
+            {
+                instances.Instances[i] = new M3Instance()
+                {
+                    FileDataID = bin.ReadUInt32(),
+                    MD5 = bin.ReadBytes(16),
+                    Unknown0 = bin.ReadUInt32(),
+                    Unknown1 = bin.ReadUInt32(),
+                    Unknown2 = bin.ReadUInt32(),
+                    Unknown3 = bin.ReadUInt32(),
+                    Unknown4 = bin.ReadUInt32(),
+                    Unknown5 = bin.ReadUInt32(),
+                    Unknown6 = bin.ReadUInt32(),
+                    Unknown7 = bin.ReadUInt32(),
+                    Unknown8 = bin.ReadUInt32(),
+                    Unknown9 = bin.ReadUInt32(),
+                    Unknown10 = bin.ReadUInt32(),
+                    Unknown11 = bin.ReadUInt32(),
+                    Unknown12 = bin.ReadUInt32(),
+                    Unknown13 = bin.ReadUInt32(),
+                    Unknown14 = bin.ReadUInt32(),
+                    Unknown15 = bin.ReadUInt32(),
+                    Unknown16 = bin.ReadUInt32()
+                };
+            }
+
+            if ((bin.BaseStream.Position - baseOffset) != instances.TotalSize)
+            {
+                Console.WriteLine("Did not end up at expect position for instances, skipping ahead..");
+                bin.BaseStream.Position = baseOffset + instances.TotalSize;
+            }
+
+            return instances;
+        }
+
+        public MES3 ReadMES3Chunk(BinaryReader bin, uint chunkSize, uint propertyA, uint propertyB)
+        {
+            var mesh = new MES3();
+
+            using (var ms = new MemoryStream())
+            using (var subbin = new BinaryReader(ms))
+            {
+                ms.Write(bin.ReadBytes((int)chunkSize), 0, (int)chunkSize);
+                subbin.BaseStream.Position = 0;
+                while (subbin.BaseStream.Position < ms.Length)
+                {
+                    var subChunkName = (M3Chunks)subbin.ReadUInt32();
+                    var debugChunkName = Encoding.ASCII.GetString(BitConverter.GetBytes((uint)subChunkName));
+                    var subChunkSize = subbin.ReadUInt32();
+
+                    Console.WriteLine("M3 chunk MES3 subchunk " + debugChunkName);
+
+                    switch (subChunkName)
+                    {
+                        case M3Chunks.M3VR:
+                            mesh.Version.Version = subbin.ReadUInt32();
+                            subbin.ReadUInt32(); // Skip unknown value
+                            break;
+                        case M3Chunks.VPOS:
+                            mesh.Vertices.Format = new string(subbin.ReadChars(4));
+                            subbin.ReadUInt32(); // Skip unknown value
+                            mesh.Vertices.Buffer = ReadBuffer(subbin, subChunkSize);
+                            break;
+                        case M3Chunks.VNML:
+                            mesh.Normals.Format = new string(subbin.ReadChars(4));
+                            subbin.ReadUInt32(); // Skip unknown value
+                            mesh.Normals.Buffer = ReadBuffer(subbin, subChunkSize);
+                            break;
+                        case M3Chunks.VUV0:
+                            mesh.UV0.Format = new string(subbin.ReadChars(4));
+                            subbin.ReadUInt32(); // Skip unknown value
+                            mesh.UV0.Buffer = ReadBuffer(subbin, subChunkSize);
+                            break;
+                        case M3Chunks.VUV1:
+                            mesh.UV1.Format = new string(subbin.ReadChars(4));
+                            subbin.ReadUInt32(); // Skip unknown value
+                            mesh.UV1.Buffer = ReadBuffer(subbin, subChunkSize);
+                            break;
+                        case M3Chunks.VUV2:
+                            mesh.UV2.Format = new string(subbin.ReadChars(4));
+                            subbin.ReadUInt32(); // Skip unknown value
+                            mesh.UV2.Buffer = ReadBuffer(subbin, subChunkSize);
+                            break;
+                        case M3Chunks.VUV3:
+                            mesh.UV3.Format = new string(subbin.ReadChars(4));
+                            subbin.ReadUInt32(); // Skip unknown value
+                            mesh.UV3.Buffer = ReadBuffer(subbin, subChunkSize);
+                            break;
+                        case M3Chunks.VUV4:
+                            mesh.UV4.Format = new string(subbin.ReadChars(4));
+                            subbin.ReadUInt32(); // Skip unknown value
+                            mesh.UV4.Buffer = ReadBuffer(subbin, subChunkSize);
+                            break;
+                        case M3Chunks.VUV5:
+                            mesh.UV5.Format = new string(subbin.ReadChars(4));
+                            subbin.ReadUInt32(); // Skip unknown value
+                            mesh.UV5.Buffer = ReadBuffer(subbin, subChunkSize);
+                            break;
+                        case M3Chunks.VTAN:
+                            mesh.Tangents.Format = new string(subbin.ReadChars(4));
+                            subbin.ReadUInt32(); // Skip unknown value
+                            mesh.Tangents.Buffer = ReadBuffer(subbin, subChunkSize);
+                            break;
+                        case M3Chunks.VINX:
+                            mesh.Indices.Format = new string(subbin.ReadChars(4));
+                            subbin.ReadUInt32(); // Skip unknown value
+                            mesh.Indices.Buffer = ReadBuffer(subbin, subChunkSize);
+                            break;
+                        case M3Chunks.VWTS:
+                            mesh.BoneWeights.IndicesFormat = new string(subbin.ReadChars(4));
+                            mesh.BoneWeights.WeightsFormat = new string(subbin.ReadChars(4));
+                            subbin.ReadUInt32(); // Skip unknown value
+                            break;
+                        case M3Chunks.VIBP:
+                            mesh.InverseBindPoses.Format = new string(subbin.ReadChars(4));
+                            subbin.ReadUInt32(); // Skip unknown value
+                            mesh.InverseBindPoses.Buffer = ReadBuffer(subbin, subChunkSize);
+                            break;
+                        case M3Chunks.VCL0:
+                            mesh.Color0.Format = new string(subbin.ReadChars(4));
+                            subbin.ReadUInt32(); // Skip unknown value
+                            mesh.Color0.Buffer = ReadBuffer(subbin, subChunkSize);
+                            break;
+                        case M3Chunks.VCL1:
+                            mesh.Color1.Format = new string(subbin.ReadChars(4));
+                            subbin.ReadUInt32(); // Skip unknown value
+                            mesh.Color1.Buffer = ReadBuffer(subbin, subChunkSize);
+                            break;
+                        case M3Chunks.VSTR:
+                            mesh.MaterialString.PropertyA = subbin.ReadUInt32();
+                            mesh.MaterialString.PropertyB = subbin.ReadUInt32();
+                            mesh.MaterialString.MaterialName = new string(subbin.ReadChars((int)subChunkSize)).Replace("\0", "");
+                            break;
+                        case M3Chunks.VGEO:
+                            mesh.Geosets.PropertyA = subbin.ReadUInt32();
+                            mesh.Geosets.PropertyB = subbin.ReadUInt32();
+                            mesh.Geosets.Geosets = new M3Geoset[mesh.Geosets.PropertyA];
+                            for(var i = 0; i < mesh.Geosets.PropertyA; i++)
+                            {
+                                mesh.Geosets.Geosets[i] = new M3Geoset()
+                                {
+                                    Unknown0 = subbin.ReadUInt32(),
+                                    Unknown1 = subbin.ReadUInt32(),
+                                    Unknown2 = subbin.ReadUInt32(),
+                                    IndicesIndex = subbin.ReadUInt32(),
+                                    IndicesCount = subbin.ReadUInt32(),
+                                    Unknown3 = subbin.ReadUInt32(),
+                                    Unknown4 = subbin.ReadUInt32(),
+                                    Unknown5 = subbin.ReadUInt32(),
+                                    Unknown6 = subbin.ReadUInt32()
+                                };
+                            }
+                            break;
+                        case M3Chunks.LODS:
+                            mesh.LodLevels.LODCount = subbin.ReadUInt32();
+                            mesh.LodLevels.GeosetCount = subbin.ReadUInt32();
+                            mesh.LodLevels.LodLevels = new M3LodLevel[mesh.LodLevels.LODCount + 1];
+                            for (var i = 0; i < mesh.LodLevels.LODCount + 1; i++)
+                            {
+                                mesh.LodLevels.LodLevels[i] = new M3LodLevel()
+                                {
+                                    IndexCount = subbin.ReadUInt32(),
+                                    VertexCount = subbin.ReadUInt32()
+                                };
+                            }
+                            break;
+                        case M3Chunks.RBAT:
+                            mesh.RenderBatches.BatchCount= subbin.ReadUInt32();
+                            mesh.RenderBatches.PropertyB = subbin.ReadUInt32();
+                            mesh.RenderBatches.RenderBatches = new M3RenderBatch[mesh.RenderBatches.BatchCount];
+                            for (var i = 0; i < mesh.RenderBatches.BatchCount; i++)
+                            {
+                                mesh.RenderBatches.RenderBatches[i] = new M3RenderBatch()
+                                {
+                                    Unknown0 = subbin.ReadUInt16(),
+                                    Unknown1 = subbin.ReadUInt16(),
+                                    Unknown2 = subbin.ReadUInt16(),
+                                    Unknown3 = subbin.ReadUInt16()
+                                };
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Unknown M3 MES3 sub-chunk: " + debugChunkName + " @ " + (bin.BaseStream.Position - chunkSize) + subbin.BaseStream.Position);
+                            subbin.BaseStream.Position += subChunkSize + 8;
+                            break;
+                    }
+                }
+            }
+
+            return mesh;
+        }
+
+        public byte[] ReadBuffer(BinaryReader bin, uint chunkSize)
+        {
+            var buffer = new byte[chunkSize];
+            bin.Read(buffer, 0, (int)chunkSize);
+            return buffer;
+        }
+
+        public M3CL ReadM3CLChunk(BinaryReader bin, uint chunkSize, uint propertyA, uint propertyB)
+        {
+            var collision = new M3CL();
+
+            bin.BaseStream.Position += chunkSize;
+
+            return collision;
+        }
+    }
+}
---
+++ WoWFormatLib/Structs/M3.Struct.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Numerics;
+using WoWFormatLib.Structs.M2;
+
+namespace WoWFormatLib.Structs.M3
+{
+    public enum M3Chunks
+    {
+        M3DT = 'M' << 0 | '3' << 8 | 'D' << 16 | 'T' << 24,
+        MES3 = 'M' << 0 | 'E' << 8 | 'S' << 16 | '3' << 24,
+        M3VR = 'M' << 0 | '3' << 8 | 'V' << 16 | 'R' << 24,
+        VPOS = 'V' << 0 | 'P' << 8 | 'O' << 16 | 'S' << 24,
+        VNML = 'V' << 0 | 'N' << 8 | 'M' << 16 | 'L' << 24,
+        VUV0 = 'V' << 0 | 'U' << 8 | 'V' << 16 | '0' << 24,
+        VUV1 = 'V' << 0 | 'U' << 8 | 'V' << 16 | '1' << 24,
+        VUV2 = 'V' << 0 | 'U' << 8 | 'V' << 16 | '2' << 24,
+        VUV3 = 'V' << 0 | 'U' << 8 | 'V' << 16 | '3' << 24,
+        VUV4 = 'V' << 0 | 'U' << 8 | 'V' << 16 | '4' << 24,
+        VUV5 = 'V' << 0 | 'U' << 8 | 'V' << 16 | '5' << 24,
+        VTAN = 'V' << 0 | 'T' << 8 | 'A' << 16 | 'N' << 24,
+        VSTR = 'V' << 0 | 'S' << 8 | 'T' << 16 | 'R' << 24,
+        VINX = 'V' << 0 | 'I' << 8 | 'N' << 16 | 'X' << 24,
+        VGEO = 'V' << 0 | 'G' << 8 | 'E' << 16 | 'O' << 24,
+        LODS = 'L' << 0 | 'O' << 8 | 'D' << 16 | 'S' << 24,
+        RBAT = 'R' << 0 | 'B' << 8 | 'A' << 16 | 'T' << 24,
+        VWTS = 'V' << 0 | 'W' << 8 | 'T' << 16 | 'S' << 24,
+        VIBP = 'V' << 0 | 'I' << 8 | 'B' << 16 | 'P' << 24,
+        VCL0 = 'V' << 0 | 'C' << 8 | 'L' << 16 | '0' << 24,
+        VCL1 = 'V' << 0 | 'C' << 8 | 'L' << 16 | '1' << 24,
+        M3CL = 'M' << 0 | '3' << 8 | 'C' << 16 | 'L' << 24,
+        CPOS = 'C' << 0 | 'P' << 8 | 'O' << 16 | 'S' << 24,
+        CNML = 'C' << 0 | 'N' << 8 | 'M' << 16 | 'L' << 24,
+        CINX = 'C' << 0 | 'I' << 8 | 'N' << 16 | 'X' << 24,
+        M3SI = 'M' << 0 | '3' << 8 | 'S' << 16 | 'I' << 24,
+        M3ST = 'M' << 0 | '3' << 8 | 'S' << 16 | 'T' << 24,
+        M3VS = 'M' << 0 | '3' << 8 | 'V' << 16 | 'S' << 24,
+        M3PT = 'M' << 0 | '3' << 8 | 'P' << 16 | 'T' << 24,
+    }
+
+    public struct M3Model
+    {
+        public M3DT Header;
+        public MES3 Mesh;
+        public M3CL Collision;
+        public M3SI Instances;
+        public M3ST StringTable;
+        public M3VS m3vs;
+        public M3XF m3xf;
+        public M3PT m3pt;
+    }
+
+    #region MES3 Mesh
+    public struct M3DT
+    {
+        public uint PropertyA;
+        public uint PropertyB;
+
+        public uint Unknown0;
+        public uint Unknown1;
+        public uint Unknown2;
+        public uint Unknown3;
+        public uint Unknown4;
+        public uint Unknown5;
+        public uint Unknown6;
+
+        public Vector3 BoundingBoxMin;
+        public Vector3 BoundingBoxMax;
+        public float Radius;
+
+        public Vector3 BoundingBox2Min;
+        public Vector3 BoundingBox2Max;
+        public float Radius2;
+
+        public uint Unknown7;
+    }
+
+    public struct MES3
+    {
+        public M3VR Version;
+        public BUFF Vertices;
+        public BUFF Normals;
+        public BUFF UV0;
+        public BUFF UV1;
+        public BUFF UV2;
+        public BUFF UV3;
+        public BUFF UV4;
+        public BUFF UV5;
+        public BUFF Tangents;
+        public VSTR MaterialString;
+        public BUFF Indices;
+        public VGEO Geosets;
+        public LODS LodLevels;
+        public RBAT RenderBatches;
+        public VWTS BoneWeights;
+        public BUFF InverseBindPoses;
+        public BUFF Color0;
+        public BUFF Color1;
+    }
+
+    public struct M3VR
+    {
+        public uint Version;
+        public uint Unknown0;
+    }
+
+    // Not a real chunk, but used for storing generic buffer data
+    public struct BUFF
+    {
+        public string Format;
+        public uint Unknown0;
+        public byte[] Buffer;
+    }
+
+    public struct VSTR
+    {
+        public uint PropertyA;
+        public uint PropertyB;
+
+        public string MaterialName;
+    }
+
+    public struct VGEO
+    {
+        public uint PropertyA;
+        public uint PropertyB;
+
+        public M3Geoset[] Geosets;
+    }
+
+    public struct M3Geoset
+    {
+        public uint Unknown0;
+        public uint Unknown1;
+        public uint Unknown2;
+        public uint IndicesIndex;
+        public uint IndicesCount;
+        public uint Unknown3;
+        public uint Unknown4;
+        public uint Unknown5;
+        public uint Unknown6;
+    }
+
+    public struct LODS
+    {
+        public uint LODCount;
+        public uint GeosetCount;
+     
+        public M3LodLevel[] LodLevels;
+    }
+
+    public struct M3LodLevel
+    {
+        public uint VertexCount;
+        public uint IndexCount;
+    }
+
+    public struct RBAT
+    {
+        public uint BatchCount;
+        public uint PropertyB;
+
+        public M3RenderBatch[] RenderBatches;
+    }
+
+    public struct M3RenderBatch
+    {
+        public uint Unknown0;
+        public uint Unknown1;
+        public uint Unknown2;
+        public uint Unknown3;
+    }
+
+    public struct VWTS
+    {
+        public string IndicesFormat;
+        public string WeightsFormat;
+        public byte[] Buffer;
+    }
+    #endregion
+
+    #region M3CL Collision
+    public struct M3CL
+    {
+        public uint PropertyA;
+        public uint PropertyB;
+        public CPOS Vertices;
+        public CNML Normals;
+        public CINX Indices;
+    }
+
+    public struct CPOS
+    {
+        public Vector3[] Vertices;
+    }
+    public struct CNML
+    {
+        public Vector3[] Normals;
+    }
+    public struct CINX
+    {
+        public ushort[] Indices;
+    }
+    #endregion
+
+    #region M3SI Instances
+    public struct M3SI
+    {
+        public uint PropertyA;
+        public uint PropertyB;
+
+        public string Magic;
+        public uint Version;
+        public uint TotalSize;
+        public uint Unknown1;
+        public uint InstanceCount;
+        public M3Instance[] Instances;
+    }
+
+    public struct M3Instance
+    {
+        public uint FileDataID;
+        public byte[] MD5;
+        public uint Unknown0;
+        public uint Unknown1;
+        public uint Unknown2;
+        public uint Unknown3;
+        public uint Unknown4;
+        public uint Unknown5;
+        public uint Unknown6;
+        public uint Unknown7;
+        public uint Unknown8;
+        public uint Unknown9;
+        public uint Unknown10;
+        public uint Unknown11;
+        public uint Unknown12;
+        public uint Unknown13;
+        public uint Unknown14;
+        public uint Unknown15;
+        public uint Unknown16;
+    }
+    #endregion
+
+    public struct M3ST
+    {
+
+    }
+
+    public struct M3VS
+    {
+
+    }
+
+    public struct M3XF
+    {
+
+    }
+
+    public struct M3PT
+    {
+    }
+}
