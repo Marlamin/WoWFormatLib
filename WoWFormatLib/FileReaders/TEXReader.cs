e11c09f03c6650959cbfaa9eba2e1ffd548d94a9
Martin Benjamins <ictmartin@gmail.com>
2026-04-11 19:23:32 +0200

    Expand TEX reading

--- WoWFormatLib/FileReaders/TEXReader.cs
+++ WoWFormatLib/FileReaders/TEXReader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 using System.Text;
 using WoWFormatLib.FileProviders;
@@ -8,49 +9,104 @@ namespace WoWFormatLib.FileReaders
 {
     public class TEXReader
     {
-        public void LoadTEX(string filename)
+        public TEXFile LoadTEX(string filename)
         {
             if (FileProvider.FileExists(filename))
             {
                 using (var tex = FileProvider.OpenFile(filename))
                 {
-                    ReadTEX(filename, tex);
+                    return ReadTEX(tex);
                 }
             }
-        }
-        private void ReadTEX(string filename, Stream tex)
-        {
-            var bin = new BinaryReader(tex);
-            long position = 0;
-            while (position < tex.Length)
+            else
             {
-                tex.Position = position;
-
-                var chunkName = (TEXChunks)bin.ReadUInt32();
-                var chunkSize = bin.ReadUInt32();
-
-                position = tex.Position + chunkSize;
-
-                switch (chunkName)
-                {
-                    case TEXChunks.TXVR:
-                        ReadTXVRChunk(bin);
-                        break;
-                    case TEXChunks.TXFN:
-                        ReadTXFNChunk(bin, chunkSize);
-                        break;
-                    case TEXChunks.TXBT:
-                    case TEXChunks.TXMD:
-                        continue;
-                    default:
-                        Console.WriteLine(string.Format("{2} Found unknown header at offset {1} \"{0}\" while we should've already read them all!", chunkName, position.ToString(), filename));
-                        break;
-                }
+                throw new FileNotFoundException();
             }
         }
-        private void ReadTXFNChunk(BinaryReader bin, uint size)
+
+        public TEXFile LoadTEX(uint fileDataID)
         {
-            //List of BLP filenames
+            if (FileProvider.FileExists(fileDataID))
+            {
+                using (var tex = FileProvider.OpenFile(fileDataID))
+                {
+                    return ReadTEX(tex);
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
+        }
+
+        private TEXFile ReadTEX(Stream tex)
+        {
+            uint version = 0;
+            var blobTextures = Array.Empty<BlobTexture>();
+            var mipMapData = Array.Empty<byte>();
+
+            using (var bin = new BinaryReader(tex))
+            {
+                long position = 0;
+
+                while (position < tex.Length)
+                {
+                    tex.Position = position;
+
+                    var chunkName = (TEXChunks)bin.ReadUInt32();
+                    var chunkSize = bin.ReadUInt32();
+
+                    position = tex.Position + chunkSize;
+
+                    switch (chunkName)
+                    {
+                        case TEXChunks.TXVR: // Version
+                            version = bin.ReadUInt32();
+                            break;
+                        case TEXChunks.TXFN: // File Names
+                            ReadTXFNChunk(bin, chunkSize); // Removed in BfA, not sure if we should keep it around for older versions?
+                            break;
+                        case TEXChunks.TXBT: // Blob Texture
+                            blobTextures = ReadTXBTChunk(bin, chunkSize);
+                            break;
+                        case TEXChunks.TXMD: // Mipmap Data
+                            mipMapData = bin.ReadBytes((int)chunkSize);
+                            break;
+                        default:
+                            Console.WriteLine(string.Format("Found unknown header at offset {1} \"{0}\" while we should've already read them all!", chunkName, position.ToString()));
+                            break;
+                    }
+                }
+            }
+
+            return new TEXFile
+            {
+                version = version,
+                blobTextures = blobTextures,
+                mipMapData = mipMapData
+            };
+        }
+
+        private static BlobTexture[] ReadTXBTChunk(BinaryReader bin, uint size)
+        {
+            var blobTextures = new BlobTexture[size / 12];
+            for (var i = 0; i < blobTextures.Length; i++)
+            {
+                blobTextures[i] = new BlobTexture
+                {
+                    fileDataID = bin.ReadUInt32(),
+                    txmdOffset = bin.ReadUInt32(),
+                    sizeX = bin.ReadByte(),
+                    sizeY = bin.ReadByte(),
+                    numMipMaps = (byte)(bin.ReadByte() & 0x7F),
+                    dxtFormat = (byte)(bin.ReadByte() & 0x0F)
+                };
+            }
+            return blobTextures;
+        }
+
+        private static void ReadTXFNChunk(BinaryReader bin, uint size)
+        {
             var blpFilesChunk = bin.ReadBytes((int)size);
 
             var str = new StringBuilder();
@@ -70,13 +126,6 @@ namespace WoWFormatLib.FileReaders
                 {
                     str.Append((char)blpFilesChunk[i]);
                 }
-            }
-        }
-        private void ReadTXVRChunk(BinaryReader bin)
-        {
-            if (bin.ReadUInt32() != 0)
-            {
-                throw new Exception("Unsupported TEX version!");
             }
         }
     }
--- WoWFormatLib/Structs/TEX.struct.cs
+++ WoWFormatLib/Structs/TEX.struct.cs
@@ -2,9 +2,26 @@
 {
     public enum TEXChunks
     {
-        TXVR = 'T' << 24 | 'X' << 16 | 'V' << 8 | 'R' << 0,
-        TXFN = 'T' << 24 | 'X' << 16 | 'F' << 8 | 'N' << 0,
-        TXBT = 'T' << 24 | 'X' << 16 | 'B' << 8 | 'T' << 0,
-        TXMD = 'T' << 24 | 'X' << 16 | 'M' << 8 | 'D' << 0,
+        TXVR = 'T' << 24 | 'X' << 16 | 'V' << 8 | 'R' << 0, // Version
+        TXFN = 'T' << 24 | 'X' << 16 | 'F' << 8 | 'N' << 0, // File Names
+        TXBT = 'T' << 24 | 'X' << 16 | 'B' << 8 | 'T' << 0, // Blob Texture
+        TXMD = 'T' << 24 | 'X' << 16 | 'M' << 8 | 'D' << 0, // Mipmap Data
+    }
+
+    public readonly struct TEXFile
+    {
+        public readonly uint version { get; init; }
+        public readonly BlobTexture[] blobTextures { get; init; }
+        public readonly byte[] mipMapData { get; init; }
+    }
+
+    public readonly struct BlobTexture
+    {
+        public readonly uint fileDataID { get; init; }
+        public readonly uint txmdOffset { get; init; }
+        public readonly byte sizeX { get; init; }
+        public readonly byte sizeY { get; init; }
+        public readonly byte numMipMaps { get; init; }
+        public readonly byte dxtFormat { get; init; }
     }
 }
