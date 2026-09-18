39585f4efdc26882787ba751000d0b2cb01ecaf4
Marlamin <ictmartin@gmail.com>
2026-09-18 12:20:13 +0200

    WIP DAT reading

---
+++ WoWFormatLib/FileReaders/DATReader.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WoWFormatLib.FileProviders;
+using WoWFormatLib.Structs;
+using WoWFormatLib.Structs.WDT;
+
+namespace WoWFormatLib.FileReaders
+{
+    public class DATReader
+    {
+        public DAT LoadDAT(uint fileDataID)
+        {
+            if (!FileProvider.FileExists(fileDataID))
+            {
+                throw new FileNotFoundException("DAT " + fileDataID + " not found!");
+            }
+
+            return LoadDAT(FileProvider.OpenFile(fileDataID));
+        }
+
+        public DAT LoadDAT(Stream datStream)
+        {
+            var datFile = new DAT();
+            datFile.chunks = new();
+            datFile.doodads = new();
+            datFile.textures = new();
+
+            using (var bin = new BinaryReader(datStream))
+            {
+                long position = 0;
+                while (position < datStream.Length)
+                {
+                    datStream.Position = position;
+
+                    var chunkName = (DATChunks)bin.ReadUInt32();
+                    var chunkSize = bin.ReadUInt32();
+
+                    position = datStream.Position + chunkSize;
+
+                    switch (chunkName)
+                    {
+                        case DATChunks.MVER: // Version
+                            datFile.version = bin.ReadUInt32();
+                            break;
+                        case DATChunks.AHDR: // Header
+                            datFile.header = bin.Read<DATHeader>();
+                            break;
+                        case DATChunks.ALOC: // Location
+                            datFile.location = bin.Read<DATLocation>();
+                            break;
+                        case DATChunks.ADOO: // Doodads
+                            datFile.doodads.Add(new string(bin.ReadChars((int)chunkSize)));
+                            break;
+                        case DATChunks.ATEX: // Textures
+                            datFile.textures.Add(new string(bin.ReadChars((int)chunkSize)));
+                            break;
+                        case DATChunks.AOCH: // ?
+                        case DATChunks.AVTX: // Vertices
+                        case DATChunks.ACVT: // Vertex colors
+                        case DATChunks.ANRM: // Normals
+                        case DATChunks.AFBO: // Flight bounds
+                            // TODO
+                            break;
+                        case DATChunks.ACNK: // Chunk (MCNK equiv)
+                            var datChunk = new DATChunk();
+                            using (var chunkStream = new MemoryStream(bin.ReadBytes((int)chunkSize)))
+                            using(var subbin = new BinaryReader(chunkStream))
+                            {
+                                if(chunkSize >= 0x40)
+                                {
+                                    datChunk.header = subbin.Read<DATMCNKHeader>();
+                                }
+                                while (subbin.BaseStream.Position < subbin.BaseStream.Length)
+                                {
+                                    var subChunkName = (DATChunks)subbin.ReadUInt32();
+                                    var subChunkSize = subbin.ReadUInt32();
+
+                                    switch (subChunkName)
+                                    {
+                                        case DATChunks.ALYR:
+                                        case DATChunks.ASHD:
+                                            subbin.BaseStream.Position += (uint)subChunkSize;
+                                            break;
+                                        default:
+                                            var debugSubChunkName = Encoding.ASCII.GetString(BitConverter.GetBytes((uint)subChunkName).Reverse().ToArray());
+                                            Console.WriteLine(string.Format("Found unknown DAT subchunk at offset {1} \"{0}\" while we should've already read them all!", debugSubChunkName, position.ToString()));
+                                            subbin.BaseStream.Position += (uint)subChunkSize;
+                                            break;
+                                    }
+                                }
+                            }
+
+                            datFile.chunks.Add(datChunk);
+                            break;
+                        default:
+                            var debugChunkName = Encoding.ASCII.GetString(BitConverter.GetBytes((uint)chunkName).Reverse().ToArray());
+                            Console.WriteLine(string.Format("Found unknown DAT chunk at offset {1} \"{0}\" while we should've already read them all!", debugChunkName, position.ToString()));
+
+                            break;
+                    }
+                }
+            }
+
+            return datFile;
+        }
+    }
+}
---
+++ WoWFormatLib/Structs/DAT.Struct.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace WoWFormatLib.Structs
+{
+    public enum DATChunks
+    {
+        MVER = 'M' << 24 | 'V' << 16 | 'E' << 8 | 'R' << 0,
+        AHDR = 'A' << 24 | 'H' << 16 | 'D' << 8 | 'R' << 0,
+        ALOC = 'A' << 24 | 'L' << 16 | 'O' << 8 | 'C' << 0,
+        AOCH = 'A' << 24 | 'O' << 16 | 'C' << 8 | 'H' << 0,
+        AVTX = 'A' << 24 | 'V' << 16 | 'T' << 8 | 'X' << 0,
+        ANRM = 'A' << 24 | 'N' << 16 | 'R' << 8 | 'M' << 0,
+        ACNK = 'A' << 24 | 'C' << 16 | 'N' << 8 | 'K' << 0,
+        ADOO = 'A' << 24 | 'D' << 16 | 'O' << 8 | 'O' << 0,
+        ACVT = 'A' << 24 | 'C' << 16 | 'V' << 8 | 'T' << 0,
+        AFBO = 'A' << 24 | 'F' << 16 | 'B' << 8 | 'O' << 0,
+        ATEX = 'A' << 24 | 'T' << 16 | 'E' << 8 | 'X' << 0,
+        ALYR = 'A' << 24 | 'L' << 16 | 'Y' << 8 | 'R' << 0,
+        ASHD = 'A' << 24 | 'S' << 16 | 'H' << 8 | 'D' << 0,
+    }
+
+    public struct DAT
+    {
+        public uint version { get; set; }
+        public DATHeader header { get; set; }
+        public DATLocation location { get; set; }
+        public List<string> doodads { get; set; }
+        public List<string> textures { get; set; }
+        public List<DATChunk> chunks { get; set; }
+    }
+
+    public struct DATLocation
+    {
+        public uint mapID { get; set; }
+        public uint adtX0 { get; set; }
+        public uint adtY0 { get; set; }
+        public uint adtX1 { get; set; }
+        public uint adtY1 { get; set; }
+    }
+
+    public struct DATHeader
+    {
+        public uint version { get; set; }
+        public uint verticesX { get; set; }
+        public uint verticesY { get; set; }
+        public uint chunksX { get; set; }
+        public uint chunksY { get; set; }
+
+        public uint unk0 { get; set; }
+        public uint unk1 { get; set; }
+        public uint unk2 { get; set; }
+        public uint unk3 { get; set; }
+        public uint unk4 { get; set; }
+        public uint unk5 { get; set; }
+        public uint unk6 { get; set; }
+        public uint unk7 { get; set; }
+        public uint unk8 { get; set; }
+        public uint unk9 { get; set; }
+        public uint unk10 { get; set; }
+    }
+
+    public struct DATChunk
+    {
+        public DATMCNKHeader header { get; set; }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct DATMCNKHeader
+    {
+        public uint indexX;
+        public uint indexY;
+        public uint flags;
+        public uint areaID;
+        public ushort holesLowRes;
+        public uint lowDetailTexturingMap0;
+        public uint lowDetailTexturingMap1;
+        public uint lowDetailTexturingMap2;
+        public uint lowDetailTexturingMap3;
+
+        public ushort MCDD;
+
+        public ushort unk0;
+        public ushort unk1;
+        public ushort unk2;
+
+        public ushort unk3;
+        public ushort unk4;
+        public ushort unk5;
+
+        public ushort unk6;
+
+        public ulong holesHighRes;
+
+        public ushort unk7;
+        public ushort unk8;
+        public ushort unk9;
+    }
+}
